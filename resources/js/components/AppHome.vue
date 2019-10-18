edf4c05be4fcebc340087074d24d0e5cacbf6c50
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-10-18 23:05:47 +0630

    Vuetify, Vue, Vue Router and Initial Components

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        <h1>Home</h1>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
     </div>
 </template>
 <script>
+import toolbar from './Toolbar'
+import AppFooter from './AppFooter'
 export default {
-    
+    components:{toolbar,AppFooter}
 }
 </script>
 <style>
