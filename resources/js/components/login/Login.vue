212e3aea0aa23c5858365c0ff0c1dc97ffb77a9e
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-10-19 00:03:25 +0630

    Login Form Created!

---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-container>
+        <v-form @submit.prevent="login">        
+        <v-text-field
+        v-model="form.email"        
+        label="E-mail"
+        required
+        ></v-text-field>
+        <v-text-field
+        v-model="form.password"
+        label="Password"
+        required
+        type="password"        
+        ></v-text-field>
+
+        <v-btn color="green" type="submit">Login</v-btn>
+    </v-form>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            form:{
+                email:null,
+                password:null
+            }
+        }
+    },
+    methods:{
+        login(){
+            axios.post('api/auth/login',this.form)
+            .then(res => console.log(res.data))
+            .error(error => console.log(error.response.data))
+        }
+    }
+}
+</script>
+<style>
+
+</style>
