58ea428239b822d147189e69b3296f57f9b97498
Gilbert Daniel <daniel.gilbert247@gmail.com>
2019-10-18 22:37:07 +0630

    Intall Vue,  Vuetify and cread AppHome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css'
+
+
+
+
+Vue.component('AppHome', require('./components/AppHome.vue').default);
+import router from './Router/router.js'
+
+const app = new Vue({
+    el: '#app',
+    router,
+    vuetify: new Vuetify()
+});
